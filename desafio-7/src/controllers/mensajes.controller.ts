667c655d4c66a79effc1a0b7cf0d14d968bdd312
Martin Marzialetti <97345870+tinchinm@users.noreply.github.com>
2022-11-12 23:16:35 -0300

    modularizado completo

--- desafio-7/src/controllers/mensajes.controller.ts
+++ desafio-7/src/controllers/mensajes.controller.ts
@@ -4,3 +4,16 @@ import path from "path";
 
 // DECLARACION DE RUTAS
 const rutaMjes = path.resolve(__dirname, '../../mensajes.json');
+
+class ApiMjs {
+    
+    async addMsg(data:any){
+        const getData = await fs.readFile(rutaMjes, 'utf-8');
+	    const mensajes = JSON.parse(getData);
+	    mensajes.push(data);
+	    await fs.writeFile(rutaMjes, JSON.stringify(mensajes, null, '\t'));
+    }
+
+}
+
+export const mensajesController = new ApiMjs();
--- desafio-7/src/services/websocket.ts
+++ desafio-7/src/services/websocket.ts
@@ -1,38 +1,35 @@
 // IMPORTACIONES DE LOS MODULOS
-import fs from "fs/promises"
-import path from "path";
 import { productController } from '../controllers/productos.controller';
+import { mensajesController } from '../controllers/mensajes.controller'
 
 const io = require ('socket.io');
-
-// DECLARACION DE RUTAS
-const rutaProd = path.resolve(__dirname, '../../productos.json');
-const rutaMjes = path.resolve(__dirname, '../../mensajes.json');
 
 // CREACIÓN DE CONSTANTE PARA EXPORTAR WS
 const initWSServer = (server:any) => {
 
+// INICIO DE WS SERVER
 const myWSServer = io(server);
 
+// CONEXION DE SOCKET
 myWSServer.on('connection', async (socket:any) => {
 
-    socket.on('productoCompleto', async (dataProd:any) => {
-        
+    //RECIBO UN PRODUCTO NUEVO Y LO GUARDO
+    socket.on('productoCompleto', async (dataProd:any) => {   
         await productController.newElement(dataProd);
     })
 
+    //ENVÍO LOS PRODUCTOS AL FRONT PARA ARMAR LA TABLA
     const getData = await productController.obtenerDatos();
     socket.emit('productos', getData)
 
+    //RECIBO LOS NUEVOS MENSAJES DEL CHAT
     socket.on('mensajeCompleto', async (data:any) => {
 
+        // ENVÍO EL MENSAJE RECIBIDO A TODOS LOS CLIENTES
         myWSServer.emit('mensaje', data)
 
-        const getData = await fs.readFile(rutaMjes, 'utf-8');
-	    const mensajes = JSON.parse(getData);
-	    mensajes.push(data);
-
-	    await fs.writeFile(rutaMjes, JSON.stringify(mensajes, null, '\t'));
+        // GRABO EL MENSAJE EN ARCHIVO DE DATOS
+        await mensajesController.addMsg(data);
 
     })
 })
